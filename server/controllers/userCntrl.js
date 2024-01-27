dd56331000e9d38ed8979060f8ee5c6b4ebe50ab
heyhimansh <himanshu_co21a4_27@dtu.ac.in>
2024-01-28 01:23:22 +0530

    routes setup

--- server/controllers/userCntrl.js
+++ server/controllers/userCntrl.js
@@ -51,101 +51,103 @@ export const bookVisit = asyncHandler(async (req, res) => {
 });
 
 
-// // funtion to get all bookings of a user
-// export const getAllBookings = asyncHandler(async (req, res) => {
-//   const { email } = req.body;
-//   try {
-//     const bookings = await prisma.user.findUnique({
-//       where: { email },
-//       select: { bookedVisits: true },
-//     });
-//     res.status(200).send(bookings);
-//   } catch (err) {
-//     throw new Error(err.message);
-//   }
-// });
+// funtion to get all bookings of a user
+export const getAllBookings = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  try {
+    const bookings = await prisma.user.findUnique({
+      where: { email },
+      select: { bookedVisits: true },
+    });
+    res.status(200).send(bookings);
+  } catch (err) {
+    throw new Error(err.message);
+  }
+});
 
 
 
-// // function to cancel the booking
-// export const cancelBooking = asyncHandler(async (req, res) => {
-//   const { email } = req.body;
-//   const { id } = req.params;
-//   try {
-//     const user = await prisma.user.findUnique({
-//       where: { email: email },
-//       select: { bookedVisits: true },
-//     });
+// function to cancel the booking
+export const cancelBooking = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  const { id } = req.params;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+      select: { bookedVisits: true },
+    });
 
-//     const index = user.bookedVisits.findIndex((visit) => visit.id === id);
+    const index = user.bookedVisits.findIndex((visit) => visit.id === id);
 
-//     if (index === -1) {
-//       res.status(404).json({ message: "Booking not found" });
-//     } else {
-//       user.bookedVisits.splice(index, 1);
-//       await prisma.user.update({
-//         where: { email },
-//         data: {
-//           bookedVisits: user.bookedVisits,
-//         },
-//       });
+    if (index === -1) {
+      res.status(404).json({ message: "Booking not found" });
+    } else {
+      user.bookedVisits.splice(index, 1);
+      await prisma.user.update({
+        where: { email },
+        data: {
+          bookedVisits: user.bookedVisits,
+        },
+      });
 
-//       res.send("Booking cancelled successfully");
-//     }
-//   } catch (err) {
-//     throw new Error(err.message);
-//   }
-// });
+      res.send("Booking cancelled successfully");
+    }
+  } catch (err) {
+    throw new Error(err.message);
+  }
+});
 
 
 
-// // function to add a resd in favourite list of a user
-// export const toFav = asyncHandler(async (req, res) => {
-//   const { email } = req.body;
-//   const { rid } = req.params;
+// function to add a resd in favourite list of a user
+export const toFav = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  const { rid } = req.params;
 
-//   try {
-//     const user = await prisma.user.findUnique({
-//       where: { email },
-//     });
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
 
-//     if (user.favResidenciesID.includes(rid)) {
-//       const updateUser = await prisma.user.update({
-//         where: { email },
-//         data: {
-//           favResidenciesID: {
-//             set: user.favResidenciesID.filter((id) => id !== rid),
-//           },
-//         },
-//       });
+    if (user.favResidenciesID.includes(rid)) {
+      const updateUser = await prisma.user.update({
+        where: { email },
+        data: {
+          favResidenciesID: {
+            set: user.favResidenciesID.filter((id) => id !== rid),
+          },
+        },
+      });
 
-//       res.send({ message: "Removed from favorites", user: updateUser });
-//     } else {
-//       const updateUser = await prisma.user.update({
-//         where: { email },
-//         data: {
-//           favResidenciesID: {
-//             push: rid,
-//           },
-//         },
-//       });
-//       res.send({ message: "Updated favorites", user: updateUser });
-//     }
-//   } catch (err) {
-//     throw new Error(err.message);
-//   }
-// });
+      res.send({ message: "Removed from favorites", user: updateUser });
+    } else {
+      const updateUser = await prisma.user.update({
+        where: { email },
+        data: {
+          favResidenciesID: {
+            push: rid,
+          },
+        },
+      });
+      res.send({ message: "Updated favorites", user: updateUser });
+    }
+  } catch (err) {
+    throw new Error(err.message);
+  }
+});
+
+
 
 // // function to get all favorites
-// export const getAllFavorites = asyncHandler(async (req, res) => {
-//   const { email } = req.body;
-//   try {
-//     const favResd = await prisma.user.findUnique({
-//       where: { email },
-//       select: { favResidenciesID: true },
-//     });
-//     res.status(200).send(favResd);
-//   } catch (err) {
-//     throw new Error(err.message);
-//   }
-// });
+export const getAllFavorites = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  try {
+    const favResd = await prisma.user.findUnique({
+      where: { email },
+      select: { favResidenciesID: true },
+    });
+    res.status(200).send(favResd);
+  } catch (err) {
+    throw new Error(err.message);
+  }
+});
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,18 +1,23 @@
 import express from "express";
 import {
   bookVisit,
-  createUser  
+  createUser,
+  getAllBookings,
+  cancelBooking,
+  toFav,
+  getAllFavorites
 } from "../controllers/userCntrl.js";
 import jwtCheck from "../config/auth0Config.js";
 const router = express.Router();
 
 router.post("/register", createUser);
-router.post("/bookVisit/:id",  bookVisit);
-// router.post("/allBookings", getAllBookings);
-// router.post("/removeBooking/:id", jwtCheck, cancelBooking);
-// router.post("/toFav/:rid", jwtCheck, toFav);
-// router.post("/allFav/", jwtCheck, getAllFavorites);
+router.post("/bookVisit/:id", bookVisit);
+router.post("/allBookings", getAllBookings);
+router.post("/removeBooking/:id", cancelBooking);
+router.post("/toFav/:rid", toFav);
+router.post("/allFav/", getAllFavorites);
 export { router as userRoute };
+
 
 
 // import express from "express";
